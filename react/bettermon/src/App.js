50478ccff2c4a5e56aef98581dcbda87e5d16b3a
Koketso M Dibakwana <Kmotsepe807@gmail.com>
2023-07-19 19:01:16 +0200

    backend dependencies

--- react/bettermon/src/App.js
+++ react/bettermon/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from './components/home/homepage';
 import Hero from './components/home/hero/hero';
@@ -7,8 +7,31 @@ import Login from './components/home/login';
 import Register from './components/home/register';
 
 const App = () => {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
-    
+    <div>
+
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading ....</p>
+      ) : (
+        data.members.map((member, i) => (
+          <p key={i}>{member}</p>
+        ))
+      )}
+
       <Router>
         <Switch>
           <Route path='/' exact component={Home} /> 
@@ -17,6 +40,7 @@ const App = () => {
           <Route path='/register' exact component={Register} /> 
         </Switch>
       </Router>
+    </div>
     
   )
 }
